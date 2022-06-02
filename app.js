e9c18f42c37c4e84d2ef0c44f99d65f036841cfe
NavnathGunjal07 <53387548+NavnathGunjal07@users.noreply.github.com>
2022-06-02 13:50:38 +0530

    add last commit

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const customMiddleware = require('./config/middleware');
 // setup the chat server to be used with socket.io
 const chatServer = require('http').Server(app);
 const chatSockets = require('./config/chat_sockets').chatSockets(chatServer);
-chatServer.listen(5000);
+// chatServer.listen(5000);
 console.log('chat server is listening on port 5000');
 const path = require('path');
 if(env.name=="development"){
